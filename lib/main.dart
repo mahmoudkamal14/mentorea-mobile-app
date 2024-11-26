6bbf3aac593e7894553e14e0a3b1d1fa893e3d12
Mahmoud Kamal <https://github.com/mahmoudkamalx14>
2024-11-26 21:12:48 +0200

    ADD design Architecture - added build page route - added spacing - added extentions - added routes - app router - build splash screen - build onboarding screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mentorea_mobile_app/core/routes/app_router.dart';
 
 import 'mentorea_app.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const MentoreaApp());
+  await ScreenUtil.ensureScreenSize();
+  runApp(MentoreaApp(appRouter: AppRouter()));
 }
--- lib/mentorea_app.dart
+++ lib/mentorea_app.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mentorea_mobile_app/core/routes/routes.dart';
+
+import 'core/routes/app_router.dart';
 
 class MentoreaApp extends StatelessWidget {
-  const MentoreaApp({super.key});
+  const MentoreaApp({super.key, required this.appRouter});
+
+  final AppRouter appRouter;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
+    double height = MediaQuery.sizeOf(context).height;
+    double width = MediaQuery.sizeOf(context).width;
+
+    return ScreenUtilInit(
+      designSize: Size(width, height),
+      minTextAdapt: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          initialRoute: Routes.splashScreen,
+          onGenerateRoute: appRouter.generateRoute,
+        );
+      },
     );
   }
 }
