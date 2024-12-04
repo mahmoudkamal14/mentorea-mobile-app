537000798c0f17f4caa714ff86462af74c5c15e6
hasscode <sona72003@gmail.com>
2024-12-04 22:24:35 +0200

    - added onboarding screen - added onboarding item - added onboarding title - added onboarding subtitle - added onboarding smooth indicator - added onboarding item model - added onboarding images

--- lib/features/onboarding/screens/onboarding_screen.dart
+++ lib/features/onboarding/screens/onboarding_screen.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import '../widgets/pageview_builder.dart';
 
-class OnboardingScreen extends StatelessWidget {
+class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
 
   @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  PageController nextPage = PageController();
+  int index =0 ;
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Color(0xff113C6C), Color(0xff051424)],
+          begin: Alignment.topLeft,
+           end: Alignment.bottomRight,
+         ),
+
+      ),
+      child: PageViewBuilder(),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mentorea_mobile_app/core/routes/app_router.dart';
-
 import 'mentorea_app.dart';
 
 void main() async {
