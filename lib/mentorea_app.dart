b28911bd9d11d5962bed88fba1d537e7dbf8b7e4
Mahmoud Kamal <https://github.com/mahmoudkamalx14>
2024-12-08 22:07:53 +0200

    Setup Localizations (android - ios) - added intl ar - added intl en - added function isArabic - update pubspec

--- lib/mentorea_app.dart
+++ lib/mentorea_app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mentorea_mobile_app/core/routes/routes.dart';
+import 'package:mentorea_mobile_app/generated/l10n.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'core/routes/app_router.dart';
 
@@ -22,6 +24,14 @@ class MentoreaApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           initialRoute: Routes.splashScreen,
           onGenerateRoute: appRouter.generateRoute,
+          locale: const Locale('ar', 'en'),
+          localizationsDelegates: const [
+            S.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: S.delegate.supportedLocales,
         );
       },
     );
