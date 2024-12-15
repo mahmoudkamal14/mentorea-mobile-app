42b6ecbb31bf4e31fcb60a4a731acfa11a38b3cb
Mahmoud Kamal <https://github.com/mahmoudkamalx14>
2024-12-15 18:29:56 +0200

    Add Bottom Navigation Bar - added mentee booking screen - added mentee commuity screen - added explore screen - added mentee profile screen - added mentee home screen - added app colors

---
+++ lib/features/home/mentee_bottom_navigation_bar_screen.dart
@@ -0,0 +1,84 @@
+import 'package:curved_labeled_navigation_bar/curved_navigation_bar.dart';
+import 'package:curved_labeled_navigation_bar/curved_navigation_bar_item.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mentorea_mobile_app/core/theme/app_colors.dart';
+import 'package:mentorea_mobile_app/core/theme/app_styles.dart';
+import 'package:mentorea_mobile_app/features/bookings/mentee_bookings_screen.dart';
+import 'package:mentorea_mobile_app/features/community/community_screen.dart';
+import 'package:mentorea_mobile_app/features/explore/explore_screen.dart';
+import 'package:mentorea_mobile_app/features/home/presentation/screens/mentee_home_screen.dart';
+import 'package:mentorea_mobile_app/features/profile/mentee_profile_screen.dart';
+
+class MenteeBottomNavigationBarScreen extends StatefulWidget {
+  const MenteeBottomNavigationBarScreen({super.key});
+
+  @override
+  State<MenteeBottomNavigationBarScreen> createState() =>
+      _BottomNavigationBarScreenState();
+}
+
+class _BottomNavigationBarScreenState
+    extends State<MenteeBottomNavigationBarScreen> {
+  final List<Widget> screens = [
+    const MenteeHomeScreen(),
+    const ExploreScreen(),
+    const MenteeBookingsScreen(),
+    const CommunityScreen(),
+    const MenteeProfileScreen(),
+  ];
+
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF9F6FD),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(bottom: 30.h),
+          child: screens[_currentIndex],
+        ),
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        height: 74.h,
+        backgroundColor: const Color(0xFFF9F6FD),
+        buttonBackgroundColor: const Color(0xFF103A69),
+        color: const Color(0xFF103A69),
+        index: _currentIndex,
+        items: [
+          CurvedNavigationBarItem(
+            child: const Icon(Icons.home_outlined, color: Colors.white),
+            label: 'Home',
+            labelStyle: AppStyles.style14RegularWhite,
+          ),
+          CurvedNavigationBarItem(
+            child: const Icon(Icons.explore_outlined, color: AppColors.white),
+            label: 'Explore',
+            labelStyle: AppStyles.style14RegularWhite,
+          ),
+          CurvedNavigationBarItem(
+            child: const Icon(Icons.access_time, color: AppColors.white),
+            label: 'Bookings',
+            labelStyle: AppStyles.style14RegularWhite,
+          ),
+          CurvedNavigationBarItem(
+            child: const Icon(Icons.people_alt_outlined, color: Colors.white),
+            label: 'Community',
+            labelStyle: AppStyles.style14RegularWhite,
+          ),
+          CurvedNavigationBarItem(
+            child: const Icon(Icons.perm_identity, color: Colors.white),
+            label: 'Profile',
+            labelStyle: AppStyles.style14RegularWhite,
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
---
+++ lib/features/home/presentation/screens/mentee_home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MenteeHomeScreen extends StatelessWidget {
+  const MenteeHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Home Screen'),
+    );
+  }
+}
